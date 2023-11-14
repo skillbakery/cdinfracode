6e26021208dd9cfdce91f717334fc6aeb29b4033
SkillBakery <contact@skillbakery.com>
2023-11-14 18:46:36 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,54 @@
+
+variable "server_count" {
+  type        = number
+  default     = 1
+  description = "The total number of VMs to create"
+}
+
+variable "name" {
+  type        = string
+  description = "The name to assign to resources in this module"
+  default     = "skillbakery-webserver"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment to assign to resource in this module"
+  default     = "prod"
+}
+
+variable "tags" {
+  type        = map(any)
+  default     = {}
+  description = "Tags to asscociate to taggable resources in this module"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "root_volume_size" {
+  type    = string
+  default = 30
+}
+
+variable "production" {
+  type    = bool
+  default = false
+}
+
+locals {
+  host = { for i in range(var.server_count) : join("-", [var.name, var.environment, i]) => {
+    index = i
+    name  = "${join("-", [var.name, var.environment, i])}"
+    }
+  }
+
+  user_data = templatefile("${path.module}/user_data.template",
+    {
+      name        = var.name
+      environment = var.environment
+  })
+
+}
