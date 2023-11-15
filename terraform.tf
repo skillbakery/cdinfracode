d1b10de6ac0c7b1c148546e5f478edb24afba707
SkillBakery <contact@skillbakery.com>
2023-11-15 13:09:55 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "~> 1.4"
 
   backend "s3" {
-    key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
-    bucket = "ADD_YOUR_BUCKET_NAME_HERE"             # the bucket
-    region = "ADD_YOUR_REGION_NAME_HERE"             # the region
+    key    = "cdinfracode/terraform.tfstate" # the directory/file.tfstate
+    bucket = "skillbakeryterraform"             # the bucket
+    region = "us-east-2"             # the region
   }
 }
